359ebde3bf0c5955fbb3f8b0ecbf114c108af004
YuweiZhan <winzhanyuwei@163.com>
2019-11-16 14:31:27 +0800

    init project

---
+++ src/test/java/com/example/securityspringboot/SecuritySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
